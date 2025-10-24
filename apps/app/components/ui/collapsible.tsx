12f831ff29ecda0742a93ca57985803d72f6af5e
Tom <tom@tomk.ca>
2025-10-24 08:49:36 +0200

    fix: correct rest parameter syntax in Collapsible wrapper components

--- apps/app/components/ui/collapsible.tsx
+++ apps/app/components/ui/collapsible.tsx
@@ -1,14 +1,14 @@
 import * as CollapsiblePrimitive from "@radix-ui/react-collapsible"
 
-function Collapsible({
-  ...props
-}: React.ComponentProps<typeof CollapsiblePrimitive.Root>) {
+function Collapsible(
+  props: React.ComponentProps<typeof CollapsiblePrimitive.Root>
+) {
   return <CollapsiblePrimitive.Root data-slot="collapsible" {...props} />
 }
 
-function CollapsibleTrigger({
-  ...props
-}: React.ComponentProps<typeof CollapsiblePrimitive.CollapsibleTrigger>) {
+function CollapsibleTrigger(
+  props: React.ComponentProps<typeof CollapsiblePrimitive.CollapsibleTrigger>
+) {
   return (
     <CollapsiblePrimitive.CollapsibleTrigger
       data-slot="collapsible-trigger"
@@ -17,9 +17,9 @@ function CollapsibleTrigger({
   )
 }
 
-function CollapsibleContent({
-  ...props
-}: React.ComponentProps<typeof CollapsiblePrimitive.CollapsibleContent>) {
+function CollapsibleContent(
+  props: React.ComponentProps<typeof CollapsiblePrimitive.CollapsibleContent>
+) {
   return (
     <CollapsiblePrimitive.CollapsibleContent
       data-slot="collapsible-content"
